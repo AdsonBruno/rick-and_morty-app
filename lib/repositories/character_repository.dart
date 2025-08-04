2c5c3d5104597ee06c2e730833064b83874df8d0
AdsonBruno <adsonbruno2@gmail.com>
2025-08-04 00:34:24 -0300

    feat: ensure that ViewModel loads the data and notify the view the state of the application

---
+++ lib/repositories/character_repository.dart
@@ -0,0 +1,5 @@
+import '../models/character_model.dart';
+
+abstract class CharacterRepository {
+  Future<List<Character>> getCharacters();
+}
---
+++ lib/view_models/character_list_view_model.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:rick_and_morty_app/models/character_model.dart';
+import 'package:rick_and_morty_app/repositories/character_repository.dart';
+
+class CharacterListViewModel extends ChangeNotifier {
+  final CharacterRepository _repository;
+
+  CharacterListViewModel({required CharacterRepository repository})
+    : _repository = repository;
+
+  List<Character> _characters = [];
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  List<Character> get characters => _characters;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> fetchCharacters() async {
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      _characters = await _repository.getCharacters();
+    } on Exception catch (e) {
+      _errorMessage = e.toString().replaceAll('Exception: ', '');
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
